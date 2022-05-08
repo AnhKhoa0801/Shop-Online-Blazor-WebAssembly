f8a9722653f1881defe851b6039c90bb27052312
Khoa Tran <khoa.tranhuynhanh@duluxgroup.com>
2022-05-09 03:00:43 +0700

    show product blazor page

--- ShopOnline.Web/Program.cs
+++ ShopOnline.Web/Program.cs
@@ -1,31 +1,14 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using ShopOnline.Web.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ShopOnline.Web;
+using ShopOnline.Web.Services;
+using ShopOnline.Web.Services.Contracts;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7252/") });
+builder.Services.AddScoped<IProductService, ProductService>();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+await builder.Build().RunAsync();
